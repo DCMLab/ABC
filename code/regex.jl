bd042d4f8adef8583b4dc42ce077b165d3032aff
Daniel Harasim <daniel.harasim@gmx.de>
2018-05-06 20:47:36 +0200

    delete parentheses in changes column of tsv tables

--- code/regex.jl
+++ code/regex.jl
@@ -5,7 +5,7 @@ regex = r"""^
     (?P<numeral>(b*|\#*)(VII|VI|V|IV|III|II|I|vii|vi|v|iv|iii|ii|i|Ger|It|Fr))
     (?P<form>[%o+M])?
     (?P<figbass>(9|7|65|43|42|2|64|6))?
-    (?P<changes>\( (\+?(b*|\#*)\d)+ \) )?
+    (\((?P<changes>(\+?(b*|\#*)\d)+)\))?
     (/\.?(?P<relativeroot>(b*|\#*)(VII|VI|V|IV|III|II|I|vii|vi|v|iv|iii|ii|i)))?
     (?P<pedalend>\])?
     (?P<phraseend>\\\\)?$
